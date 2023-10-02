cc84c42a319b0b18a7ca648f16fc0f7d74078a1d
Rohit Raj <mr.rajarohit9431@gmail.com>
2023-10-02 20:48:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -417,7 +417,7 @@ app.post("/api/verifyotp/:phone", async (req, res) => {
     })
     .catch((error) => {
       console.error("Error sending OTP:", error);
-      res.status(200).json({ message: "fake otp", 1737});
+      res.status(200).json({ message: "fake otp", otp: 1737 });
       // res.status(500).json({ message: "Failed to send OTP" });
     });
 });
